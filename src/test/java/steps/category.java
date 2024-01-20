146f1d038beb4be59e8a048decb189692733d59f
1523209 <saulUAB@outlook.com>
2024-01-20 19:26:59 +0100

    Another feature added

---
+++ src/test/java/steps/category.java
@@ -0,0 +1,34 @@
+package steps;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class category {
+	WebDriver driver;
+	WebDriverWait wait;
+	
+	@Before
+	public void SetUp() throws Exception {
+		driver = SetUp.getDriver();
+		wait = SetUp.getWait();
+	}
+	
+	@Given("^the user clicks the (.*) button")
+	public void userClicksCategory(String Category) {
+		driver.findElement(By.linkText(Category)).click();
+	}
+	@Then("^the user lands in the correct (.*) page")
+	public void didUserLandInTheCorrectPage(String Product) throws InterruptedException {
+		Thread.sleep(1000);
+		
+		String name = driver.findElement(By.xpath("/html/body/div[5]/div/div[2]/div/div[1]/div/div/h4/a")).getText();
+		Assert.assertTrue(name.matches(Product));
+	}
+	
+}
---
+++ target/test-classes/features/categories.feature
@@ -0,0 +1,11 @@
+Feature: Categories
+Scenario Outline: Check Categories
+Given the user is in the index page
+When the user clicks the <Category> button
+Then the user lands in the correct <Product> page
+
+Examples:
+|Category|Product|
+|Phones|Samsung galaxy s6|
+|Laptops|Sony vaio i5|
+|Monitors|Apple monitor 24|
